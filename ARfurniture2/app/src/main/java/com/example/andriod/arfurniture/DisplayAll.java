e19561bdff99e1fbe7a74762664ca5d255ecf0fe
s3633197 <s3633197@student.rmit.edu.au>
2020-09-25 15:50:25 +0800

    basic draft

---
+++ ARfurniture2/app/src/main/java/com/example/andriod/arfurniture/DisplayAll.java
@@ -0,0 +1,13 @@
+package com.example.andriod.arfurniture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayAll extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_all);
+    }
+}
